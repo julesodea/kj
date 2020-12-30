3a3529f852c41eb80f0bf019311854e5d7db71af
julesodea <jules.odea@gmail.com>
2020-12-30 14:59:22 +1300

    Nav created

---
+++ components/Nav.jsx
@@ -0,0 +1,12 @@
+import nav from '../styles/Nav.module.scss';
+
+export default function Home() {
+  return (
+    <nav className={nav.nav}>
+      <h1>Katherine Jack</h1>
+      <ul>
+        <li>hello</li>
+      </ul>
+    </nav>
+  );
+}
