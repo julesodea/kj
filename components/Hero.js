c2438edbc8dedebc2e3e7ec737f36c6e27c8e0d7
julesodea <jules.odea@gmail.com>
2021-01-02 11:48:43 +1300

    next image used for landing

--- components/Hero.js
+++ components/Hero.js
@@ -1,10 +1,23 @@
 import hero from '../styles/Hero.module.scss';
+import Image from 'next/image';
 
 export default function Hero({ img }) {
   return (
     <section className={hero.section}>
-      <div className={hero.img} src={img} alt='image'>
-        <h1>Coming soon. ✌️ </h1>
+      <Image
+        src='/image.jpg'
+        alt='Picture of the author'
+        width={500}
+        height={250}
+        // width={100}
+        // sizes='responsive'
+        objectFit='cover'
+        layout='responsive'
+        // height={500}
+      />
+
+      <div className={hero.img}>
+        <h1>Coming soon. </h1>
       </div>
     </section>
   );
